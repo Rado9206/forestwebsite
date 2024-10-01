8c535a3da1717eb27b0e09531af4432a31c8c00f
Rado <rj.airoo@gmail.com>
2024-10-01 11:52:52 +0200

    edited styles contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
+const sentMsgBtn = document.querySelector('.contact__form-btn button-basic-style')
+const msgStatus = document.querySelector('.contact__msg-status')
 const footerYear = document.querySelector('.footer__year')
+
+const checkMailStatus = () => {
+	if (document.location.search === '?mail_status=sent') {
+		msgStatus.classList.add('success')
+		msgStatus.textContent = 'Wiadomość została wysłana!'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('success')
+		}, 3000)
+	}
+	if (document.location.search === '?mail_status=error') {
+		msgStatus.classList.add('error')
+		msgStatus.textContent = 'Wiadomość nie została wysłana!'
+
+		setTimeout(() => {
+			msgStatus.classList.remove('error')
+		}, 3000)
+	}
+}
+
+//można też to zrobić z URLSearchParams()
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
@@ -6,3 +29,5 @@ const handleCurrentYear = () => {
 }
 
 handleCurrentYear()
+
+sentMsgBtn.addEventListener('click', checkMailStatus)
